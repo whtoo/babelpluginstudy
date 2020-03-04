4ab8f9022d881287ede5ac6495ccef91f8f83084
blitz.whtoo <whtoo@qq.com>
2020-03-04 21:31:22 +0800

    1. 使用replaceWith实现节点重写

--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ traverse(ast, {
     enter(path) {
         if(path.isIdentifier({name : 'n'})) {
             path.node.name = 'x'
-        } else if(path.isReturnStatement()){
-            if(path.node.argument.type === 'BinaryExpression'){
-                    console.log('enter binary expr')
-                    path.node.argument = t.binaryExpression("*",t.identifier('x'),t.numericLiteral(2))
-            }            
+        } else if(path.isBinaryExpression()){
+            if(path.node.operator === '+'){
+                /// ReplaceWith 就是对当前path做"替换"的方法
+                path.replaceWith(t.binaryExpression("*",t.identifier('x'),t.numericLiteral(2)))
+            }
         }
     }
 })
